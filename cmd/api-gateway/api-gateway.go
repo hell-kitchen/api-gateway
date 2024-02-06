e8b96d319fd9e4b9bbffa8b62c463b2671252f74
Vlad Marlo <vlad.marlo@ya.ru>
2024-02-06 23:30:43 +0300

    added http server DI integration

--- cmd/api-gateway/api-gateway.go
+++ cmd/api-gateway/api-gateway.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"github.com/hell-kitchen/api-gateway/internal/config"
+	"github.com/hell-kitchen/api-gateway/internal/controller/http"
 	"go.uber.org/fx"
+	"go.uber.org/zap"
 )
 
 func main() {
@@ -15,6 +17,18 @@ func NewOptions() fx.Option {
 	return fx.Options(
 		fx.Provide(
 			config.NewServer,
+			http.NewServer,
+			zap.NewProduction,
+		),
+		fx.Invoke(
+			addServerStartup,
 		),
 	)
+}
+
+func addServerStartup(lc fx.Lifecycle, server *http.Server) {
+	lc.Append(fx.Hook{
+		OnStart: server.OnStart,
+		OnStop:  server.OnStop,
+	})
 }
