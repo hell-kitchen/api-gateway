7564d44fc1642f0ce009f570f323ecc976694318
Vlad Marlo <vlad.marlo@ya.ru>
2024-02-07 00:49:25 +0300

    added routes placeholders

---
+++ internal/controller/http/handlers.go
@@ -0,0 +1,99 @@
+package http
+
+import "github.com/labstack/echo/v4"
+
+func (srv *Server) IngredientsGetAll(ctx echo.Context) error {
+	panic("not implemented")
+}
+
+func (srv *Server) IngredientsGetByID(ctx echo.Context) error {
+	panic("not implemented")
+}
+
+func (srv *Server) RecipesGetAll(ctx echo.Context) error {
+	panic("not implemented")
+}
+
+func (srv *Server) RecipesCreate(ctx echo.Context) error {
+	panic("not implemented")
+}
+
+func (srv *Server) RecipesGetByID(ctx echo.Context) error {
+	panic("not implemented")
+}
+
+func (srv *Server) RecipesUpdateByID(ctx echo.Context) error {
+	panic("not implemented")
+}
+
+func (srv *Server) RecipesDeleteByID(ctx echo.Context) error {
+	panic("not implemented")
+}
+
+func (srv *Server) RecipesDownloadShoppingCart(ctx echo.Context) error {
+	panic("not implemented")
+}
+
+func (srv *Server) RecipesAddRecipeToShoppingCart(ctx echo.Context) error {
+	panic("not implemented")
+}
+
+func (srv *Server) RecipesDeleteRecipeFromShoppingCart(ctx echo.Context) error {
+	panic("not implemented")
+}
+
+func (srv *Server) RecipesAddRecipeToFavorite(ctx echo.Context) error {
+	panic("not implemented")
+}
+
+func (srv *Server) RecipesDeleteRecipeFromFavorite(ctx echo.Context) error {
+	panic("not implemented")
+}
+
+func (srv *Server) TagsGetAll(ctx echo.Context) error {
+	panic("not implemented")
+}
+
+func (srv *Server) TagsGetOne(ctx echo.Context) error {
+	panic("not implemented")
+}
+
+func (srv *Server) TokensLogin(ctx echo.Context) error {
+	panic("not implemented")
+}
+
+func (srv *Server) TokensLogout(ctx echo.Context) error {
+	panic("not implemented")
+}
+
+func (srv *Server) UsersGetAll(ctx echo.Context) error {
+	panic("not implemented")
+}
+
+func (srv *Server) UsersCreate(ctx echo.Context) error {
+	panic("not implemented")
+}
+
+func (srv *Server) UsersGetByID(ctx echo.Context) error {
+	panic("not implemented")
+}
+
+func (srv *Server) UsersGetMe(ctx echo.Context) error {
+	panic("not implemented")
+}
+
+func (srv *Server) UsersSetPassword(ctx echo.Context) error {
+	panic("not implemented")
+}
+
+func (srv *Server) UsersGetSubscriptions(ctx echo.Context) error {
+	panic("not implemented")
+}
+
+func (srv *Server) UsersSubscribe(ctx echo.Context) error {
+	panic("not implemented")
+}
+
+func (srv *Server) UsersUnsubscribe(ctx echo.Context) error {
+	panic("not implemented")
+}
--- internal/controller/http/server.go
+++ internal/controller/http/server.go
@@ -77,5 +77,50 @@ func (srv *Server) configureMiddlewares() {
 }
 
 func (srv *Server) configureRoutes() {
+	apiRouter := srv.router.Group("/api")
+	usersRouter := apiRouter.Group("/users")
+	{
+		// Users subgroup
+		usersRouter.GET("/", srv.UsersGetAll)
+		usersRouter.POST("/", srv.UsersCreate)
+		usersRouter.GET("/me", srv.UsersGetMe)
+		usersRouter.GET("/:id", srv.UsersGetByID)
+		usersRouter.POST("/set_password", srv.UsersSetPassword)
 
+		// Subscriptions subgroup
+		usersRouter.GET("/subscriptions", srv.UsersGetSubscriptions)
+		usersRouter.POST("/:id/subscribe", srv.UsersSubscribe)
+		usersRouter.POST("/:id/subscribe", srv.UsersUnsubscribe)
+	}
+	tokensRouter := apiRouter.Group("/auth/token")
+	{
+		tokensRouter.POST("/login", srv.TokensLogin)
+		tokensRouter.POST("/logout", srv.TokensLogin)
+	}
+	tagsRouter := apiRouter.Group("/tags")
+	{
+		tagsRouter.GET("/", srv.TagsGetAll)
+		tagsRouter.GET("/:id", srv.TagsGetOne)
+	}
+	recipesRouter := apiRouter.Group("/recipes")
+	{
+		recipesRouter.GET("/", srv.RecipesGetAll)
+		recipesRouter.POST("/", srv.RecipesCreate)
+		recipesRouter.GET("/:id", srv.RecipesGetByID)
+		recipesRouter.PATCH("/:id", srv.RecipesUpdateByID)
+		recipesRouter.DELETE("/:id", srv.RecipesDeleteByID)
+
+		recipesRouter.GET("/download_shopping_cart", srv.RecipesDownloadShoppingCart)
+		recipesRouter.POST("/:id/shopping_cart", srv.RecipesAddRecipeToShoppingCart)
+		recipesRouter.DELETE("/:id/shopping_cart", srv.RecipesDeleteRecipeFromShoppingCart)
+
+		recipesRouter.POST("/:id/favorite", srv.RecipesAddRecipeToFavorite)
+		recipesRouter.DELETE("/:id/favorite", srv.RecipesDeleteRecipeFromFavorite)
+	}
+
+	ingredientsRouter := apiRouter.Group("/ingredients")
+	{
+		ingredientsRouter.GET("/", srv.IngredientsGetAll)
+		ingredientsRouter.GET("/:id", srv.IngredientsGetByID)
+	}
 }
