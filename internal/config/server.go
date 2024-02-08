b57161555d45ffa20581a0bbd7ef89294f1921f3
Vlad Marlo <vlad.marlo@ya.ru>
2024-02-09 02:15:20 +0300

    added new configurations to server

--- internal/config/server.go
+++ internal/config/server.go
@@ -3,12 +3,17 @@ package config
 import (
 	"context"
 	"fmt"
+	"time"
 )
 
 // Server is config to set on which port server will be running.
 type Server struct {
-	BindAddr string `config:"bind-addr,short=a"`
-	BindPort int    `config:"bind-port,short=p"`
+	BindAddr   string        `config:"bind-addr,short=a"`
+	BindPort   int           `config:"bind-port,short=p"`
+	BindHost   string        `config:"bind-host"`
+	Timeout    time.Duration `config:"server-timeout"`
+	AllowHosts []string      `config:"allow-hosts"`
+	fullAddr   string
 }
 
 // NewServer return new Server config.
@@ -18,13 +23,28 @@ func NewServer() (*Server, error) {
 	s := &Server{
 		BindAddr: "localhost",
 		BindPort: 8080,
+		BindHost: "localhost",
+		AllowHosts: []string{
+			"localhost",
+			"127.0.0.1",
+			"0.0.0.0",
+		},
+		Timeout: time.Second,
 	}
 	if err := getConfitaLoader().Load(ctx, s); err != nil {
 		return nil, fmt.Errorf("error while loading config: %w", err)
 	}
+	s.fullAddr = fmt.Sprintf("http://%s", s.BindHost)
 	return s, nil
 }
 
+// Addr return address on which application will be started at.
+//
+// Use Addr while configuring HTTP server.
 func (cfg Server) Addr() string {
 	return fmt.Sprintf("%s:%d", cfg.BindAddr, cfg.BindPort)
+}
+
+func (cfg Server) FullAddr() string {
+	return cfg.fullAddr
 }
