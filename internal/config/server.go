f460d452eff5ad49d19081611d6385020812800b
Vlad Marlo <vlad.marlo@ya.ru>
2024-02-13 16:31:24 +0300

    added docs for config structure

--- internal/config/server.go
+++ internal/config/server.go
@@ -3,19 +3,41 @@ package config
 import (
 	"context"
 	"fmt"
-	"github.com/hell-kitchen/pkg/confita"
 	"time"
+
+	"github.com/hell-kitchen/pkg/confita"
 )
 
-// Server is config to set on which port server will be running.
+// Server is config of HTTP Server.
 type Server struct {
-	BindAddr   string        `config:"bind-addr,short=a"`
-	BindPort   int           `config:"bind-port,short=p"`
-	BindHost   string        `config:"bind-host"`
-	UseHTTPS   bool          `config:"https"`
-	Timeout    time.Duration `config:"server-timeout"`
-	AllowHosts []string      `config:"allow-hosts"`
-	fullAddr   string
+	// BindAddr is Host address
+	// Default: localhost
+	BindAddr string `config:"bind-addr,short=a"`
+	// BindPort is port on which application will be running at.
+	// By default is 8080
+	BindPort int `config:"bind-port,short=p"`
+	// BindHost is external address of system.
+	//
+	// Do not add protocol in front of this parameter.
+	// Bad: https://example.org http://example.org
+	// Good: example.org
+	//
+	// If you set it to outer domain address and set UseHTTPS to true
+	// then application will autogenerate cert file to use LetsEncrypt encryption.
+	BindHost string `config:"bind-host"`
+	// UseHTTPS is bool attribute to run HTTPS server instea of HTTP.
+	// If setted to true then it will generate autocert file for you.
+	UseHTTPS bool `config:"https"`
+	// Timeout is timeout of server to wait for response.
+	Timeout time.Duration `config:"server-timeout"`
+	// AllowHosts is used in CORS configurations.
+	AllowHosts []string `config:"allow-hosts"`
+	// fullAddr is internal attribute to store address on which Server
+	// will be running at.
+	// Creating autimaticly on creation by BindAddr and BindHost.
+	//
+	// By default is "localhost:8080"
+	fullAddr string
 }
 
 // NewServer return new Server config.
@@ -49,6 +71,7 @@ func (cfg Server) Addr() string {
 	return fmt.Sprintf("%s:%d", cfg.BindAddr, cfg.BindPort)
 }
 
+// FullAddr return external address of server.
 func (cfg Server) FullAddr() string {
 	return cfg.fullAddr
 }
