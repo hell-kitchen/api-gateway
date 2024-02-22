9a5820fc59bbe9090d60bdbe566b25fef68797ab
Vlad Marlo <vlad.marlo@ya.ru>
2024-02-23 01:45:10 +0300

    added docs to config

--- internal/config/ingredients.go
+++ internal/config/ingredients.go
@@ -2,16 +2,25 @@ package config
 
 import (
 	"context"
+
 	"github.com/hell-kitchen/pkg/confita"
 )
 
+// Ingredients is configuration of Ingredients Client.
 type Ingredients struct {
-	Addr     string `config:"ingredients-addr"`
-	UseTLS   bool   `config:"ingredients-use-tls"`
+	// Addr is full address to connect to Ingredients server.
+	//
+	// e.g. localhost:5000 or example.org
+	Addr string `config:"ingredients-addr"`
+	// UseTLS is used to use TLS transport credentials in work with service.
+	UseTLS bool `config:"ingredients-use-tls"`
+	// CertFile is location of CertFile.
 	CertFile string `config:"ingredients-cert-file"`
-	KeyFile  string `config:"ingredients-key-file"`
+	// KeyFile is location of KeyFile.
+	KeyFile string `config:"ingredients-key-file"`
 }
 
+// NewIngredients creates new Ingredients config and Loads data from env/flag to it.
 func NewIngredients() (*Ingredients, error) {
 	cfg := &Ingredients{
 		Addr:     ":5000",
