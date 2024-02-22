220783d82852dfcc9a33831135c2c1655561046e
Vlad Marlo <vlad.marlo@ya.ru>
2024-02-23 01:20:48 +0300

    refactoring in main package

--- cmd/api-gateway/api-gateway.go
+++ cmd/api-gateway/api-gateway.go
@@ -1,17 +1,14 @@
 package main
 
 import (
-	"context"
 	"github.com/hell-kitchen/api-gateway/internal/config"
 	"github.com/hell-kitchen/api-gateway/internal/controller/http"
 	"github.com/hell-kitchen/api-gateway/internal/service"
 	"github.com/hell-kitchen/api-gateway/internal/service/fake"
+	ingredientsService "github.com/hell-kitchen/api-gateway/internal/service/production/ingredients"
 	tagsService "github.com/hell-kitchen/api-gateway/internal/service/production/tags"
-	pb "github.com/hell-kitchen/api-gateway/pkg/api/proto/tags"
-	"github.com/hell-kitchen/api-gateway/pkg/client/tags"
 	"github.com/hell-kitchen/pkg/logger"
 	"go.uber.org/fx"
-	"go.uber.org/zap"
 )
 
 func main() {
@@ -25,25 +22,26 @@ func NewOptions() fx.Option {
 		fx.Provide(
 			config.NewServer,
 			config.NewTags,
+			config.NewIngredients,
 			http.NewServer,
 			logger.NewProduction,
 			tagsService.New,
+			ingredientsService.New,
 			createTagsClient,
+			createIngredientsClient,
 			fx.Annotate(service.New, fx.As(new(service.Interface))),
 		),
 		fx.Invoke(
 			fake.NewRecipes,
 			fake.NewTokens,
-			fake.NewIngredients,
 			fake.NewUsers,
+
 			applyTagsService,
+			applyIngredientService,
 
 			addServerStartup,
 		),
 		fx.NopLogger,
-		//fx.WithLogger(func(log *zap.Logger) fxevent.Logger {
-		//	return &fxevent.ZapLogger{Logger: log}
-		//}),
 	)
 }
 
@@ -51,24 +49,8 @@ func applyTagsService(srv service.Interface, tag *tagsService.Service) {
 	srv.ApplyTags(tag)
 }
 
-func createTagsClient(lc fx.Lifecycle, log *zap.Logger, cfg *config.Tags) (res pb.TagsServiceClient, err error) {
-	var cli *tags.Client
-
-	cli, err = tags.New(cfg)
-	if err != nil {
-		return
-	}
-	resp, err := cli.GetAll(context.Background(), &pb.GetAllRequest{})
-	log.Info("got request", zap.Any("cfg", cfg), zap.Any("response-tags", resp.GetTags()), zap.Error(err))
-
-	lc.Append(fx.Hook{
-		OnStart: func(ctx context.Context) error {
-			zap.L().Info("tags client on start just called")
-			return nil
-		},
-		OnStop: cli.Stop,
-	})
-	return cli, nil
+func applyIngredientService(srv service.Interface, ingredients *ingredientsService.Service) {
+	srv.ApplyIngredients(ingredients)
 }
 
 func addServerStartup(lc fx.Lifecycle, server *http.Server) {
